fda34f5b73547c04e8c09be170e1ef01a51b386f
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-05-14 04:46:53 +0700

    done create

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  List dataBarang = [];
+  var namaBarang = "";
+  var jenisBarang = "";
+  var deskripsiBarang = "";
+
+  Future<void> readDataGudang() async {
+    final connect = await MySqlConnection.connect(ConnectionSettings(
+        host: '10.0.2.2', port: 3306, user: 'root', db: 'pbbmod12'));
+    try {
+      var result = await connect.query('SELECT * FROM gudang');
+      setState(() {
+        dataBarang.clear();
+        for (var row in result) {
+          dataBarang.add(row);
+          print(row['namaBarang']);
+        }
+      });
+    } catch (e) {
+      print("Error reading data: $e");
+    } finally {
+      await connect.close();
+    }
+  }
+
+  Future<void> createDataGudang() async {
+    final connect = await MySqlConnection.connect(ConnectionSettings(
+        host: '10.0.2.2', port: 3306, user: 'root', db: 'pbbmod12'));
+
+    try {
+      await connect.query(
+        'INSERT INTO gudang (namaBarang, jenisBarang, deskripsiBarang) VALUES (?, ?, ?)',
+        [namaBarang, jenisBarang, deskripsiBarang],
+      );
+    } catch (e) {
+      print("Error inserting data: $e");
+    } finally {
+      await connect.close();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readDataGudang();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Data Gudang",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: dataBarang.isEmpty
+                ? Center(child: Text("Tidak ada Barang"))
+                : ListView.builder(
+                    itemCount: dataBarang.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                        decoration:
+                            BoxDecoration(color: Colors.white, boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 0.5,
+                            blurRadius: 3,
+                            offset: Offset(0, 1),
+                          ),
+                        ]),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                dataBarang[index]['namaBarang'],
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                dataBarang[index]['jenisBarang'],
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black.withOpacity(0.5)),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  backgroundColor: Colors.white,
+                  title: Text('Tambah Data Gudang'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Masukkan Nama Barang',
+                          labelStyle:
+                              TextStyle(color: Colors.black.withOpacity(0.60)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                                color: Colors.black.withOpacity(0.60)),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          namaBarang = value;
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        onChanged: (value) {
+                          jenisBarang = value;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Masukkan Kategori Barang',
+                          labelStyle:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                                color: Colors.black.withOpacity(0.60)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                            ),
+                            onPressed: () async {
+                              if (namaBarang != "" &&
+                                  jenisBarang != "" &&
+                                  deskripsiBarang != "") {
+                                await createDataGudang();
+                                await readDataGudang();
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            child: Text(
+                              "Save",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+          child: Icon(Icons.add),
+          foregroundColor: Colors.white,
+          shape: CircleBorder(),
+          backgroundColor: Colors.blue,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
